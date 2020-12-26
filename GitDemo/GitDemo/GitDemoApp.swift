bc44d4b6bd23dfd7fe030cc7490fbf1ad8db2c13
sachingupta <sendtosachinigupta@gmail.com>
2020-12-26 01:09:53 -0800

    Initial Commit for Git Demo Sample

---
+++ GitDemo/GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Sachin Gupta on 12/26/20.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
